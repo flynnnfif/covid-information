99558d7f89bcbec1a4e0df3d396e8086a8542c85
flynnnfif <92360714+flynnnfif@users.noreply.github.com>
2022-04-13 22:17:24 +0800

    Add files via upload

--- covidui.py
+++ covidui.py
@@ -10,14 +10,14 @@ app = QApplication(sys.argv)
 dlg = QDialog()
 dlg.setWindowTitle('covid')
 dlg.resize(500, 500)
+  
+btn_con = QPushButton(dlg)
+btn_con.resize(200, 50)
+btn_con.setText('全国疫情数据')
+btn_con.move(100, 100)
 
-btn = QPushButton(dlg)
-btn.resize(200, 50)
-btn.setText('click it!')
-btn.move(100, 100)
 f = open("prov.txt", "r",encoding="utf-8")
 combo = QComboBox(dlg)
-
 for line in f.readlines():
     line=line.strip()
     combo.addItem(line)
